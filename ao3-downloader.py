b6a50de3d5f481f975e4fd55b25481068e7b5197
Nooa Peltokangas <nooa.peltokangas@pm.me>
2020-01-20 00:54:16 +0200

    initial url generation and download

--- ao3-downloader.py
+++ ao3-downloader.py
@@ -1,6 +1,17 @@
 import requests
+from bs4 import BeautifulSoup
 
-res = requests.get("https://archiveofourown.org/downloads/22247407/All My Angel Wants.html")
+url = "https://archiveofourown.org/works/16571744/chapters/38830805"
+
+res = requests.get(url)
+soup = BeautifulSoup(res.text, 'html.parser')
+title = soup.find(class_="title heading")
+title = str(title)
+stripped = title.strip('<h2 class="title heading">/').strip()
+workid = url.strip('https://archiveofourown.org/works/').strip('/chapters/38830805')
+finalurl = "https://archiveofourown.org/downloads/" + workid + "/" + stripped + ".html"
+
+res = requests.get(finalurl)
 playfile = open('test.html', 'wb')
 for chunk  in res.iter_content(100000):
     playfile.write(chunk)
