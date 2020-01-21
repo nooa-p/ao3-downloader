3ff37b64b4b0c1a75f179c3d9ffeffe8b07baa43
Nooa Peltokangas <nooa.peltokangas@pm.me>
2020-01-21 16:18:55 +0200

    initial download with new method

--- ao3-downloader.py
+++ ao3-downloader.py
@@ -1,20 +1,19 @@
 import requests
 import sys
 from bs4 import BeautifulSoup
-from selenium import webdriver
+
+def check_url():
+    return
+
 
 url = "https://archiveofourown.org/works/16571744/chapters/38830805"
 
 res = requests.get(url)
 soup = BeautifulSoup(res.text, 'html.parser')
-title = soup.find(class_="title heading")
-title = str(title)
-stripped = title.strip('<h2 class="title heading">/').strip()
-workid = url.strip('https://archiveofourown.org/works/').strip('/chapters/38830805')
-finalurl = "https://archiveofourown.org/downloads/" + workid + "/" + stripped + ".html"
+finalurl = str(soup.find(class_="download").find("a", string="HTML")).strip('<a href=">HTML/')
 
-res = requests.get(finalurl)
-playfile = open((stripped + '.html'), 'wb')
+res = requests.get("https://archiveofourown.org/" + finalurl)
+playfile = open(('testi.html'), 'wb')
 for chunk  in res.iter_content(100000):
     playfile.write(chunk)
 playfile.close()
