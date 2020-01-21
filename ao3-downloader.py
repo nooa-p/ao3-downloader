0b90c81e59b655e4454e6e34e58663a2368d8d2a
Nooa Peltokangas <nooa.peltokangas@pm.me>
2020-01-21 17:19:45 +0200

    added function to check url is in correct form

--- ao3-downloader.py
+++ ao3-downloader.py
@@ -2,18 +2,42 @@ import requests
 import sys
 from bs4 import BeautifulSoup
 
-def check_url():
-    return
+def check_url(url):
+    if not "archiveofourown.org/works/" in url:
+        sys.exit("error: incorrect url type / not ao3 link")
+    if "https://www." in url:
+        if len(url) == 38:
+            sys.exit("error: incorrect url type / too short no id(httpswww)")
+        if not url[38].isnumeric():
+            sys.exit("error: incorrect url type / no id")
+        else:
+            return url
+    if "https://" in url:
+        if len(url) == 34:
+            sys.exit("error: incorrect url type / too short no id(https)")
+        if not url[34].isnumeric():
+            sys.exit("error: incorrect url type / no id")
+        else:
+            return url
+    else:
+        if len(url) == 26:
+            sys.exit("error: incorrect url type / too short no id(nothing)")
+        if not url[26].isnumeric():
+            sys.exit("error: incorrect url type / no id")
+        else:
+            return "https://" + url
 
 
-url = "https://archiveofourown.org/works/16571744/chapters/38830805"
+#url = "https://archiveofourown.org/works/23"
 
-res = requests.get(url)
-soup = BeautifulSoup(res.text, 'html.parser')
-finalurl = str(soup.find(class_="download").find("a", string="HTML")).strip('<a href=">HTML/')
+#print(check_url(url))
 
-res = requests.get("https://archiveofourown.org/" + finalurl)
-playfile = open(('testi.html'), 'wb')
-for chunk  in res.iter_content(100000):
-    playfile.write(chunk)
-playfile.close()
+#res = requests.get(url)
+#soup = BeautifulSoup(res.text, 'html.parser')
+#finalurl = str(soup.find(class_="download").find("a", string="HTML")).strip('<a href=">HTML/')
+
+#res = requests.get("https://archiveofourown.org/" + finalurl)
+#playfile = open(('testi.html'), 'wb')
+#for chunk  in res.iter_content(100000):
+#    playfile.write(chunk)
+#playfile.close()
